e9028f452df51046713dd1413eb0ae123321d0de
BaselX <Basel.x33x@gmail.com>
2024-08-05 21:04:07 +0200

    Testing Communication  Between Client and Host

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkPlayer : NetworkBehaviour
 {
     
+    NetworkVariable<int> m_Scroe = new NetworkVariable<int>(0,NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public GameObject m_SpawnBullet = null;
 
 
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+
+        m_Scroe.OnValueChanged += (oldValue, newValue) =>
+        {
+            Debug.Log( "Player:  " + OwnerClientId + "   |  Score: " + newValue);
+        };
+
+    }
     void Update()
     {
-        if (!IsOwner) return;
+
+
+
+        if (!IsOwner) { return; }
+
+
+
+
+        //TestServerRPC();
+        TestClientRPC();
+
+
 
 
         Vector3 Movment = Vector3.zero; 
@@ -24,5 +50,40 @@ public class NetworkPlayer : NetworkBehaviour
         
         transform.position += Movment * Time.deltaTime * 5f;
         
+    }
+
+    [ServerRpc]
+    void TestServerRPC ()
+    {
+        //if (Input.GetKey(KeyCode.Q)) m_Scroe.Value++;
+        //if (Input.GetKey(KeyCode.E)) m_Scroe.Value--;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            var bullet = Instantiate(m_SpawnBullet, transform.position, Quaternion.identity);
+
+            bullet.GetComponent<NetworkObject>().Spawn(true);
+
+        }
+
+    }
+
+
+    [ClientRpc]
+    void TestClientRPC()
+    {
+        //if (Input.GetKey(KeyCode.Q)) m_Scroe.Value++;
+        //if (Input.GetKey(KeyCode.E)) m_Scroe.Value--;
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            var bullet = Instantiate(m_SpawnBullet, transform.position, Quaternion.identity);
+
+            bullet.GetComponent<NetworkObject>().Spawn(true);
+
+        }
     }
 }
